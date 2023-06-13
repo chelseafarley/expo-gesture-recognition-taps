4f5d01c638419b9ce9c484d88ba6c3545e703f44
Chelsea <chelsea@tripwiretech.com>
2023-06-13 19:58:38 +1200

    Gesture taps

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+
+// npx expo install react-native-gesture-handler
 
 export default function App() {
+  const singleTap = Gesture.Tap().onEnd((_event, success) => {
+    if (success) {
+      console.log("Single tap");
+    }
+  });
+
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onEnd((_event, success) => {
+      if (success) {
+        console.log("Double tap");
+      }
+    });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <GestureDetector gesture={Gesture.Race(doubleTap, singleTap)}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </GestureDetector>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
